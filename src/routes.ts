b8356eded68acca90d77a1d53102fc70b52844d6
Oleh Hryb <oleg.grib.ua@gmail.com>
2025-06-30 02:50:41 +0100

    fix: 🐛 removed joy.id code

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,6 @@ import type { RouteDefinition } from '@solidjs/router'
 
 import { Home } from './pages/home'
 import { Root } from './pages/root'
-import { SendEth } from './pages/send'
-import { SendERC20 } from './pages/send-erc20'
-import { SignMessage } from './pages/sign-message'
-import { SignTypeData } from './pages/sign-typed-data'
-import { Redirect } from './pages/redirect'
 import MerchantLogin from './pages/merchant/index'
 import MerchantCreate from './pages/merchant/create'
 import MerchantRedemption from './pages/merchant/redemption'
@@ -20,26 +15,6 @@ export const routes: RouteDefinition[] = [
   {
     path: '/home',
     component: Home,
-  },
-  {
-    path: '/send',
-    component: SendEth,
-  },
-  {
-    path: '/send-erc20',
-    component: SendERC20,
-  },
-  {
-    path: '/sign-message',
-    component: SignMessage,
-  },
-  {
-    path: '/sign-typed-data',
-    component: SignTypeData,
-  },
-  {
-    path: '/redirect',
-    component: Redirect,
   },
   {
     path: '**',
